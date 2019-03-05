255cb0c220d043ebc04208432a0391126d272ff4
unknown <u67v67@gmail.com>
2019-03-05 22:23:20 +0200

    Gui

--- AutomaticHwChecker/MainWindow.xaml.cs
+++ AutomaticHwChecker/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace AutomaticHwChecker
 {
@@ -20,9 +22,60 @@ namespace AutomaticHwChecker
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string HomeworkSolutionsDirectoryPath => HomeworkSolutionsDirectoryTextbox.Text;
+        private string InputOutputDirectoryPath => InputOutputDirectoryTextbox.Text;
+        private string DesktopPath => Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+
         public MainWindow()
         {
             InitializeComponent();
+            this.HomeworkSolutionsDirectoryTextbox.TextChanged += (sender, args) => RunEnableCheck();
+            this.InputOutputDirectoryTextbox.TextChanged += (sender, args) => RunEnableCheck();
+            this.ChooseHomeworkDirectoryButton.Click += (sender, args) => ChooseHomeworkDirectory();
+            this.ChooseInputOutputDirectoryButton.Click += (sender, args) => ChooseInputOutputDirectory();
+        }
+
+        private void ChooseHomeworkDirectory()
+        {
+            if (GetDirectoryFromUser(out string directory, "Homework Directory"))
+                HomeworkSolutionsDirectoryTextbox.Text = directory;
+        }
+
+        private void ChooseInputOutputDirectory()
+        {
+            if (GetDirectoryFromUser(out string directory, "Input-Output Directory"))
+                InputOutputDirectoryTextbox.Text = directory;
+        }
+
+        private bool GetDirectoryFromUser(out string directoryPath, string title)
+        {
+            directoryPath = null;
+            var dlg = new CommonOpenFileDialog();
+            dlg.Title            = title;
+            dlg.IsFolderPicker   = true;
+
+            dlg.AddToMostRecentlyUsedList = false;
+            dlg.AllowNonFileSystemItems   = false;
+            dlg.EnsureFileExists          = true;
+            dlg.EnsurePathExists          = true;
+            dlg.EnsureReadOnly            = false;
+            dlg.EnsureValidNames          = true;
+            dlg.Multiselect               = false;
+            dlg.ShowPlacesList            = true;
+
+            if (dlg.ShowDialog() == CommonFileDialogResult.Ok)
+            { 
+                directoryPath = dlg.FileName;
+                return true;
+            }
+
+            return false;
+        }
+
+
+        private void RunEnableCheck()
+        {
+            RunButton.IsEnabled = Directory.Exists(HomeworkSolutionsDirectoryPath) && Directory.Exists(InputOutputDirectoryPath);
         }
     }
 }
